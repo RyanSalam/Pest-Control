3c32bbf2aee65d6159b8f04dc8d19c5390e15266
RyanSalam <Ryan@agni.com>
2021-01-11 17:08:07 -0500

    Scripts Addition

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+
+    Ability ability;
+    string abilityName;
+    private void Start()
+    {
+        
+    }
+}
