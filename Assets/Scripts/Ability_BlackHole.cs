01bd0440b01f84f020e5ea72557db9a7b485a8d2
Kevin Vickers <dreammchine64@hotmail.com>
2021-01-13 14:52:17 -0500

    Ability_Blackhole adjustment/initialization

--- Assets/Scripts/Ability_BlackHole.cs
+++ Assets/Scripts/Ability_BlackHole.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Ability_BlackHole : Ability
 {
 
-    //public GameObject prefabToSpawn;
+    public GameObject prefabToSpawn;
+    public Actor_Player pA;
+    
     // Start is called before the first frame update
     public override void Execute()
     {
-        throw new System.NotImplementedException();
-         
+        Instantiate(prefabToSpawn, pA.AbilitySpawnPoint.position, pA.AbilitySpawnPoint.rotation);
+        
     }
 
     public override void Initialize(GameObject abilitySource)
     {
+        pA = abilitySource.GetComponent<Actor_Player>();
         throw new System.NotImplementedException();
     }
 }
