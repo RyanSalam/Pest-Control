d57aaac6aba65c22f80a6f83c1a1331a3c1e83e4
Joshua Smith <55558076+OhmikronStudios@users.noreply.github.com>
2021-01-13 13:51:34 -0500

    Added Audio and a couple of basic Cues to test
    Audio System
    Pistol Fire
    Rifle Fire
    Charge Fire/Charge
    Trap Place

---
+++ Assets/Audio/Scripts/AudioCue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCue : MonoBehaviour
+{
+	[Header("Sound definition")]
+	[SerializeField] private AudioCueSO _audioCue = default;
+	[SerializeField] private bool _playOnStart = false;
+
+	[Header("Configuration")]
+	[SerializeField] private AudioCueEventChannelSO _audioCueEventChannel = default;
+	[SerializeField] private AudioConfigurationSO _audioConfiguration = default;
+
+	private void Start()
+	{
+		if (_playOnStart)
+			PlayAudioCue();
+	}
+
+	public void PlayAudioCue()
+	{
+		_audioCueEventChannel.RaiseEvent(_audioCue, _audioConfiguration, transform.position);
+	}
+}
