71d26b4106a18393af02a6caaa3acd49feadf7fe
RyanSalam <Ryan@agni.com>
2021-01-18 11:37:38 -0500

    GameManager & Timer Implementation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    public static Actor_Player selectedPlayer;
+    public static string SelectedLevel;
 
+    public void Restart()
+    {
+        string s = SceneManager.GetActiveScene().name;
+        LoadScene(s);
+    }
+
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void SetLevel(string name)
+    {
+        SelectedLevel = name;
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,17 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Timer : MonoBehaviour
+public class Timer
 {
-    // Start is called before the first frame update
-    void Start()
+    private float remainingSeconds = 0f;
+    private float totalSeconds;
+
+    private float playBackSpeed = 1f;
+
+    bool playing = true;
+    bool reverse = false;
+
+    public bool isPlaying
     {
+        get { return playing; }
     }
 
-    // Update is called once per frame
-    void Update()
+    // This is a non monobehavior function. 
+    // This to decrease the number of components a game object will have without coupling all the code together.
+    // To make a timer it's basically var myTimer = new Timer(howmuchTimeYouwant);
+    // When a parameter is already declared with a value, that parameter becomes optional.
+    public Timer(float duration, bool playOnStart = true)
     {
-        
+        totalSeconds = duration;
+        playing = playOnStart;
     }
+
+    public void Tick(float rate)
+    {
+        if (!playing) return;
+
+        remainingSeconds += rate * playBackSpeed;
+
+        if (remainingSeconds >= totalSeconds)
+        {
+            OnTimerEnd?.Invoke();
+        }
+    }
+
+    public void SetPlayBackSpeed(float speed)
+    {
+        playBackSpeed = speed;
+    }
+
+    public void PlayFromStart()
+    {
+        remainingSeconds = 0f;
+        playing = true;
+    }
+
+    public void Stop()
+    {
+        playing = false;
+    }
+
+    public float GetProgress()
+    {
+        return remainingSeconds / totalSeconds;
+    }
+
+    public event Action OnTimerEnd;
 }
