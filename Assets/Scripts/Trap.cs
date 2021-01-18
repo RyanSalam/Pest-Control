e18399bfa668ebbd0b744912cc8d82851ef7cbee
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-01-18 15:51:23 -0500

    added base trap script

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap : MonoBehaviour
+public class Trap : Weapon
 {
-    // Start is called before the first frame update
-    void Start()
+    public Item item;
+    public float forwardOffset;
+
+    [SerializeField] protected int buildSpace = 1;
+    [SerializeField] protected LayerMask whatIsBuildable;
+    [SerializeField] protected int trapDamage = 1;
+    [SerializeField] protected int maxUses;
+    [HideInInspector] protected float movementSpeed;
+
+    public virtual bool Buildable()
     {
-        
+        Collider[] objects = Physics.OverlapBox(transform.position, Vector3.one * 0.4f);
+
+        foreach (Collider col in objects)
+        {
+            if (col.GetComponent<Actor>() == true && col != this.GetComponent<Collider>())
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void Build()
     {
-        
+        enabled = true;
+        transform.parent = null;
+        GetComponent<AudioCue>().PlayAudioCue(); //for audio
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, Vector3.one * 0.6f);
+        Gizmos.color = Color.red;
+    }
+
+    public virtual void Interact(Actor_Player player)
+    {
+        //player energy refund here
     }
 }
