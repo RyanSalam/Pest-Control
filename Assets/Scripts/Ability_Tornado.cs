b207905fbfd2e66224b9c801664287e89924423c
DoganKara <dcankara.uni@gmail.com>
2021-01-18 23:32:40 -0500

    Tornado Ability Script Setup
    Added TornadoStrike() and AoEDamageSequence()

--- Assets/Scripts/Ability_Tornado.cs
+++ Assets/Scripts/Ability_Tornado.cs
@@ -2,17 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability_Tornado : MonoBehaviour
+[CreateAssetMenu(menuName = "Abilities/Tornado Strike Ability")]
+public class Ability_Tornado : Ability
 {
-    // Start is called before the first frame update
-    void Start()
+    public Actor_Player pA;
+
+    [SerializeField] private float tornadoStrikeForwardMomentum = 10f;
+    [SerializeField] private float tornadoStrikeAoERange = 6f;
+    [SerializeField] private float tornadoStrikeDamagePerSecond;
+
+    [SerializeField] LayerMask whatisEnemy;
+    [SerializeField] Collider[] enemiesHit;
+
+    public override void Execute()
     {
+        TornadoStrike();
+    }
+
+    public override bool CanExecute()
+    {
+        return base.CanExecute();
+    }
+
+    public override void Initialize(GameObject abilitySource)
+    {
+        pA = abilitySource.GetComponent<Actor_Player>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TornadoStrike()
     {
-        
+        // Move forward
+        pA.Controller.Move(pA.transform.forward * tornadoStrikeForwardMomentum * Time.deltaTime);
+        // Detect enemies within the range
+        enemiesHit = Physics.OverlapSphere(pA.transform.position, tornadoStrikeAoERange, whatisEnemy);
+
+        if (enemiesHit.Length > 0)
+        {
+            // Execute AoEDamageSequence() 
+            AoEDamageSequence();
+        }
     }
+
+    // The following function needs to be called on Update()?
+    public void AoEDamageSequence()
+    {
+
+        var data = new DamageData
+        {
+            damageAmount = Mathf.RoundToInt(tornadoStrikeDamagePerSecond * Time.deltaTime),
+            damager = pA,
+        };
+
+        foreach (Collider enemy in enemiesHit)
+        {
+            enemy.gameObject.GetComponent<Actor_Enemy>().TakeDamage(data);
+            // Following line is for testing purposes
+            //Debug.Log(enemy.name + ": " + enemy.gameObject.GetComponent<DamageBody>().CurrentHealth + " health");
+        }
+
+    }
+
 }
