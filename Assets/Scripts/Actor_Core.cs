1f64de3d0debb01a49814f297a839529463c1873
CrevanEvansense <60749547+CrevanEvansense@users.noreply.github.com>
2021-01-18 20:42:48 -0500

    Wave Manager
    Created Wave Manager and updated Actor_Enemy, Timer and Actor_Core. Added a level manager to my personal scene and created a temporary enemy prefab, for use in the Wave Manager.

--- Assets/Scripts/Actor_Core.cs
+++ Assets/Scripts/Actor_Core.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Actor_Core : MonoBehaviour
+public class Actor_Core : Actor
 {
     // Start is called before the first frame update
     void Start()
