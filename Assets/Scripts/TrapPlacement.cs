c2d642d228e96d028f6ec66a33cbbf514156f0fa
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-01-20 13:48:42 -0500

    added stuff to my scene and added trap rotation and placement code

--- Assets/Scripts/TrapPlacement.cs
+++ Assets/Scripts/TrapPlacement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class TrapPlacement : MonoBehaviour, IEquippable
 {
-    [SerializeField] protected LayerMask whatIsBuildable;
-    
+    [SerializeField] private GameObject trapToSpawn;
+    [SerializeField] protected float offset;
+    [SerializeField] protected float verticalSearch; // max distanace for raycast 
+    [SerializeField] protected LayerMask whatIsBuildable; // detecting groundlayer for raycast 
+    [SerializeField] protected int trapPrice;
     public void Equip()
     {
-      
+        gameObject.SetActive(true); //setting trap gameobject to active when equiping 
     }
+
+    public virtual void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(offset, LevelManager.Instance.Player.transform.position.y, 0); //setting offset in front of the player
+
+        RaycastHit outHit;
+        Ray floorCast = new Ray(transform.position, Vector3.down); // cast from trapObject to spawn 
+        Physics.Raycast(floorCast, out outHit, verticalSearch, whatIsBuildable);
+        transform.position = outHit.collider.transform.position; // assigns the trap to where the raycast hits
+    }
+
     public void PrimaryFire()
     {
-        throw new System.NotImplementedException();
+        trapToSpawn.transform.position = transform.position;
+        trapToSpawn.SetActive(true);
     }
 
     public bool PrimaryFireCheck()
@@ -22,7 +42,7 @@ public class TrapPlacement : MonoBehaviour, IEquippable
 
     public void SecondaryFire()
     {
-        throw new System.NotImplementedException();
+        transform.Rotate(transform.rotation.eulerAngles + Quaternion.Euler(0, 90, 0).eulerAngles); //rotating trap by 90 degrees clockwise
     }
 
     public bool SecondaryFireCheck()
@@ -32,6 +52,6 @@ public class TrapPlacement : MonoBehaviour, IEquippable
 
     public void Unequip()
     {
-        throw new System.NotImplementedException();
+        gameObject.SetActive(false);
     }
 }
