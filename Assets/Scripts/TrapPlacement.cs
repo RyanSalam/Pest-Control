e9ee454abc57257d3490f8f71b7ab7d2427ad68c
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-01-20 23:55:47 -0500

    added trap placement code

--- Assets/Scripts/TrapPlacement.cs
+++ Assets/Scripts/TrapPlacement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class TrapPlacement : MonoBehaviour, IEquippable
 {
-    [SerializeField] protected LayerMask whatIsBuildable;
+    [SerializeField] private GameObject trapToSpawn;
+    [SerializeField] protected float offset;
+    [SerializeField] protected float verticalSearch; //max distance for raycast
+    [SerializeField] protected LayerMask whatIsBuildable; // detecting groundlayer for raycast 
+    [SerializeField] protected int trapPrice;
+    public bool isDebugging;
     
+    public virtual void Awake()
+    {
+        //gameObject.SetActive(false);
+    }
+
     public void Equip()
     {
-      
+        transform.SetParent(LevelManager.Instance.Player.WeaponHolder);
+        transform.localPosition = Vector3.zero; //resetting gameObject's position
+        gameObject.SetActive(true); //setting trap to activate when equiping 
     }
+
+    private void Update()
+    {
+        transform.localPosition = new Vector3(offset, LevelManager.Instance.Player.transform.position.y, 0); //setting the offset of trap in front of player
+
+        RaycastHit outHit;
+        Ray floorCast = new Ray(transform.localPosition, Vector3.down); //cast from trap to spawn 
+        Physics.Raycast(floorCast, out outHit, verticalSearch, whatIsBuildable);
+        transform.localPosition = outHit.collider.transform.localPosition; // assigning the trap to raycast hit position
+    }
+
     public void PrimaryFire()
     {
-        throw new System.NotImplementedException();
+        //setting the trap GameObject to spawn on raycast's position 
+        trapToSpawn.transform.position = transform.localPosition;
+        trapToSpawn.SetActive(true);
     }
 
     public bool PrimaryFireCheck()
@@ -22,7 +47,7 @@ public class TrapPlacement : MonoBehaviour, IEquippable
 
     public void SecondaryFire()
     {
-        throw new System.NotImplementedException();
+        transform.Rotate(transform.rotation.eulerAngles + Quaternion.Euler(0, 90, 0).eulerAngles); //rotating trap by 90 degrees clockwise 
     }
 
     public bool SecondaryFireCheck()
@@ -32,6 +57,14 @@ public class TrapPlacement : MonoBehaviour, IEquippable
 
     public void Unequip()
     {
-        throw new System.NotImplementedException();
+        gameObject.SetActive(false); //setting trap to deActivate when unEquipping 
+    }
+
+    protected void OnDrawGizmos()
+    {
+        if(isDebugging)
+        {
+            Debug.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - verticalSearch, transform.position.z), Color.green);
+        }
     }
 }
