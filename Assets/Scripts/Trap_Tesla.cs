f62fedeac85a203401c80adb4489e75ca85c392b
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-01-22 00:54:13 -0500

    added sprite and name to scriptable objects

--- Assets/Scripts/Trap_Tesla.cs
+++ Assets/Scripts/Trap_Tesla.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap_Tesla : MonoBehaviour
+public class Trap_Tesla : Trap
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool isChaining = false;
+    private int currentChainAmmount = 0; // deafult chain value 
+    [SerializeField] private int enemyChainAmmount = 4; // setting how much enemy can be infliceted by lighting chain
+    [SerializeField] private float chainRadius = 2.0f; //raidus of the chain
+    [SerializeField] private LayerMask whatIsEnemy; //check enemy layer 
+
+    void ChainLighting(Vector3 chainPosition)
     {
-        
+        Collider[] objects = Physics.OverlapSphere(chainPosition, chainRadius,)
     }
 
     // Update is called once per frame
