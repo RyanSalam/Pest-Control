fe03e7205480747702b0123fe22f94e201c19cbd
RyanSalam <Ryan@agni.com>
2021-01-22 00:45:55 -0500

    Shop UI functionality

---
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using TMPro;
+
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image BorderImage = null;
+    [SerializeField] private Image ItemIcon = null;
+    [SerializeField] private TMP_Text itemName = null;
+    [SerializeField] private TMP_Text itemCost = null;
+
+    [SerializeField] private Item item;
+
+    [SerializeField] private Sprite selectedSprite = null;
+    [SerializeField] private Sprite unselectedSprite = null;
+
+    private bool purchased = false;
+
+    private void Start()
+    {
+        itemName.text = item.itemName;
+        ItemIcon.sprite = item.itemIcon;
+
+        itemCost.text = item.itemCost.ToString();
+    }
+
+    public void BuyItem()
+    {
+        if (LevelManager.Instance.InventoryList.Contains(item)) return;
+
+        LevelManager.Instance.InventoryAdd(item);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one * 1.25f, 0.25f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(Vector3.one, 0.25f);
+    }
+}
---
+++ Assets/Scripts/UI/ShopUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] private Text customerEnergy = null;
+    [SerializeField] private Text itemDescription = null;
+
+    [SerializeField] private GameObject inventoryPanel = null;
+
+    private void Start()
+    {
+        LevelManager.Instance.onItemChangeCallback += UpdateItemUI;
+
+        ToggleMenu();
+    }
+
+    public void ToggleMenu()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+
+        Time.timeScale = gameObject.activeSelf ? 0.0f : 1.0f;
+        Cursor.lockState = gameObject.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = gameObject.activeSelf;
+
+        LevelManager.Instance.Player.controlsEnabled = !gameObject.activeSelf;
+    }
+
+    private void UpdateItemUI()
+    {
+        throw new NotImplementedException();
+    }
+}
