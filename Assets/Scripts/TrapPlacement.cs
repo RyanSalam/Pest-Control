ddad2e6f055009bf413e8e7a061022875dac52e2
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-01-21 21:11:52 -0500

    Trap Placement implemented

--- Assets/Scripts/TrapPlacement.cs
+++ Assets/Scripts/TrapPlacement.cs
@@ -5,39 +5,44 @@ using UnityEngine;
 public class TrapPlacement : MonoBehaviour, IEquippable
 {
     [SerializeField] private GameObject trapToSpawn;
+    public Transform trapModel;
     [SerializeField] protected float offset;
     [SerializeField] protected float verticalSearch; //max distance for raycast
     [SerializeField] protected LayerMask whatIsBuildable; // detecting groundlayer for raycast 
     [SerializeField] protected int trapPrice;
     public bool isDebugging;
+
     
     public virtual void Awake()
     {
-        //gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     public void Equip()
     {
         transform.SetParent(LevelManager.Instance.Player.WeaponHolder);
-        transform.localPosition = Vector3.zero; //resetting gameObject's position
+        transform.localPosition = Vector3.zero + transform.forward * offset; //resetting gameObject's position
         gameObject.SetActive(true); //setting trap to activate when equiping 
     }
 
     private void Update()
     {
-        transform.localPosition = new Vector3(offset, LevelManager.Instance.Player.transform.position.y, 0); //setting the offset of trap in front of player
+        //transform.position = new Vector3(offset, LevelManager.Instance.Player.transform.position.y, 0); //setting the offset of trap in front of player
 
         RaycastHit outHit;
-        Ray floorCast = new Ray(transform.localPosition, Vector3.down); //cast from trap to spawn 
-        Physics.Raycast(floorCast, out outHit, verticalSearch, whatIsBuildable);
-        transform.localPosition = outHit.collider.transform.localPosition; // assigning the trap to raycast hit position
+        Ray floorCast = new Ray(transform.position, Vector3.down); //cast from trap to spawn 
+        Debug.DrawRay(transform.position, Vector3.down, Color.green);
+        if (Physics.Raycast(floorCast, out outHit, verticalSearch, whatIsBuildable))
+        {
+           trapModel.position = outHit.transform.position; // assigning the trap to raycast hit position
+        }
+       
     }
 
     public void PrimaryFire()
     {
         //setting the trap GameObject to spawn on raycast's position 
-        trapToSpawn.transform.position = transform.localPosition;
-        trapToSpawn.SetActive(true);
+        GameObject tempTrap = Instantiate(trapToSpawn, trapModel.position, transform.rotation); //instantiating trap 
     }
 
     public bool PrimaryFireCheck()
