afccba9e670e0edc710c1c50f743e68ccf1512b5
Joshua Smith <55558076+OhmikronStudios@users.noreply.github.com>
2021-01-25 11:33:14 -0500

    Comments and reorganization for clarity

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -24,11 +24,12 @@ public class SettingsManager : MonoBehaviour
     void Start()
     {
         AM = FindObjectOfType<AudioManager>();
+        
+        //search the system for possible resolutions available and populate the drop down with that list.
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
         resolutions = Screen.resolutions;
         int currentResolutionIndex = 0;
-
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " +
@@ -38,12 +39,14 @@ public class SettingsManager : MonoBehaviour
                   && resolutions[i].height == Screen.currentResolution.height)
                 currentResolutionIndex = i;
         }
-
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.RefreshShownValue();
+
+        //If player preferences exist, set their settings.
         LoadSettings(currentResolutionIndex);
     }
 
+    //Setter functions for settings values.
     public void SetMasterVolume(float volume)
     {
         AM.SetGroupVolume("Master", volume);
@@ -118,41 +121,29 @@ public class SettingsManager : MonoBehaviour
     //    qualityDropdown.value = qualityIndex;
     //}
 
-    public void SaveSettings()
+    
+    public void SaveSettings()  //Save the current setting values to the player preferences
     {
-        //PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value); Debug.Log("QualitySettingPreference:" + qualityDropdown.value);
         PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value); Debug.Log("ResolutionPreference:" + resolutionDropdown.value);
-        //PlayerPrefs.SetInt("TextureQualityPreference", textureDropdown.value); Debug.Log("TextureQualityPreference:" + textureDropdown.value);
-        //PlayerPrefs.SetInt("AntiAliasingPreference", aaDropdown.value); Debug.Log("AntiAliasingPreference:" + aaDropdown.value);
         PlayerPrefs.SetInt("FullscreenPreference", Convert.ToInt32(Screen.fullScreen));
         PlayerPrefs.SetFloat("MasterVolumePreference", currentMasterVolume); Debug.Log("MasterVolumePreference:" + currentMasterVolume);
         PlayerPrefs.SetFloat("MusicVolumePreference", currentMusicVolume); Debug.Log("MusicVolumePreference:" + currentMusicVolume);
         PlayerPrefs.SetFloat("SFXVolumePreference", currentSFXVolume); Debug.Log("SFXVolumePreference:" + currentSFXVolume);
-        
+
+        //PlayerPrefs.SetInt("QualitySettingPreference", qualityDropdown.value); Debug.Log("QualitySettingPreference:" + qualityDropdown.value);
+        //PlayerPrefs.SetInt("TextureQualityPreference", textureDropdown.value); Debug.Log("TextureQualityPreference:" + textureDropdown.value);
+        //PlayerPrefs.SetInt("AntiAliasingPreference", aaDropdown.value); Debug.Log("AntiAliasingPreference:" + aaDropdown.value);
+
     }
 
-    public void LoadSettings(int currentResolutionIndex)
+    public void LoadSettings(int currentResolutionIndex) //Called at start based on the player preferences
     {
-        //if (PlayerPrefs.HasKey("QualitySettingPreference"))
-        //    qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
-        //else
-        //    qualityDropdown.value = 3;
 
         if (PlayerPrefs.HasKey("ResolutionPreference"))
             resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
         else
             resolutionDropdown.value = currentResolutionIndex;
-
-        //if (PlayerPrefs.HasKey("TextureQualityPreference"))
-        //    textureDropdown.value = PlayerPrefs.GetInt("TextureQualityPreference");
-        //else
-        //    textureDropdown.value = 0;
-
-        //if (PlayerPrefs.HasKey("AntiAliasingPreference"))
-        //    aaDropdown.value = PlayerPrefs.GetInt("AntiAliasingPreference");
-        //else
-        //    aaDropdown.value = 1;
-
+        
         if (PlayerPrefs.HasKey("FullscreenPreference"))
             Screen.fullScreen = Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
         else
@@ -172,6 +163,21 @@ public class SettingsManager : MonoBehaviour
             SFXVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolumePreference");
         else
             SFXVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolumePreference");
+        
+        //if (PlayerPrefs.HasKey("QualitySettingPreference"))
+        //    qualityDropdown.value = PlayerPrefs.GetInt("QualitySettingPreference");
+        //else
+        //    qualityDropdown.value = 3;
+        //if (PlayerPrefs.HasKey("TextureQualityPreference"))
+        //    textureDropdown.value = PlayerPrefs.GetInt("TextureQualityPreference");
+        //else
+        //    textureDropdown.value = 0;
+        //if (PlayerPrefs.HasKey("AntiAliasingPreference"))
+        //    aaDropdown.value = PlayerPrefs.GetInt("AntiAliasingPreference");
+        //else
+        //    aaDropdown.value = 1;
+
+
     }
 
 
