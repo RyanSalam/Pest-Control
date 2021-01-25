f16c771c83b725947c99ff337c713f4bf4a1429a
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-01-25 00:05:22 -0500

    changed tesla again

--- Assets/Scripts/Trap_Tesla.cs
+++ Assets/Scripts/Trap_Tesla.cs
@@ -19,6 +19,17 @@ public class Trap_Tesla : Trap
         {
             ChainLighting(transform.position); // ressetting chain lighting position
         }
+        // if the enemy is not empty
+        if (enemyTarget != null)
+        {
+            Activate();
+        }
+        else
+        {
+            // resetting chain back to normal
+            isChaining = false;
+            currentChainAmmount = 0;
+        }
     }
 
     void ChainLighting(Vector3 chainPosition)
@@ -36,22 +47,7 @@ public class Trap_Tesla : Trap
                 currentChainAmmount = 0;
                 return;
             }
-
             enemyTarget = objects[0].GetComponent<Actor_Enemy>(); //setting enemy as objects
-            // if the enemy is not empty
-            if (enemyTarget != null)
-            {
-                //make enemy take damage from the trap
-                enemyTarget.TakeDamage(trapDamage);
-                //add the current chain ammount when it is an enemy
-                currentChainAmmount++;         
-            }
-            else
-            {
-                // resetting chain back to normal
-                isChaining = false;
-                currentChainAmmount = 0;
-            }
         }
     }
 
@@ -64,17 +60,10 @@ public class Trap_Tesla : Trap
 
         //set the chain to true and get the lighting to that enemy
         isChaining = true;
-        ChainLighting(enemyTarget.transform.position); //set the lighting of the trap to the enemys position
+        ChainLighting(enemyTarget.transform.position); //set the lighting of the trap to the enemys position                                                      
+        enemyTarget.TakeDamage(trapDamage); //make enemy take damage from the trap
+        currentChainAmmount++;  //add the current chain ammount when it is an enemy
         base.Activate();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        //checkig if collider is an enemy and checks if the trap chain is off
-        if (enemyTarget !=null && isChaining == false)
-        {
-            Activate();
-        }
     }
 
     protected override void OnDrawGizmos()
