10db1906f142d8cabe29d19d3dfb54e31c688ab3
Joshua Smith <55558076+OhmikronStudios@users.noreply.github.com>
2021-01-30 18:09:46 -0500

    Main Panel & Character Panel

---
+++ Assets/CanvasUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TurnObjectOn(GameObject obj)
+    {
+            obj.SetActive(true);
+        Debug.Log("clicked");
+    }
+
+    public void TurnObjectOff(GameObject obj)
+    {
+      
+            obj.SetActive(false);
+
+    }
+
+    public void Quit()
+    {
+        FindObjectOfType<GameManager>().OnQuitButton();
+    }
+
+    
+
+    
+}
