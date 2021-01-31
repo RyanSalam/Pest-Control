908c0b1664b963b115148fdde104e6cf081652ee
DoganKara <dcankara.uni@gmail.com>
2021-01-30 19:28:23 -0500

    HUD UI updates
    - Enemy count and core health bar updated
    - Added core icon & wave number info to HUD
    - Edited HUDUI.cs
    - Added following getters: MaxHealth (Actor.cs / for core), WaveNumber (WaveManager.cs), EnemiesRemaining (WaveManager.cs)
    - Edited UI prefab

--- Assets/Scripts/UI/HUDUI.cs
+++ Assets/Scripts/UI/HUDUI.cs
@@ -2,18 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HUDUI : MonoBehaviour
 {
-    [SerializeField] Text waveNumber;
     // Wave management
     // Update wave number - called on a new wave
     /// <summary>
     /// Update the current wave. Call at start of a new wave. Takes in current wave number
     /// </summary>
-    public void UpdateWaveNumber(int wave)
+    [SerializeField] TMP_Text waveNumber;
+
+    Actor_Core core;
+    [SerializeField] Image coreFill;
+    [SerializeField] TMP_Text enemyCount;
+
+    private void Start()
     {
-        waveNumber.text = wave.ToString();
+        core = LevelManager.Instance.Core;
     }
+
+    private void Update()
+    {
+        UpdateCoreHealth();
+        UpdateEnemyCount();
+        UpdateWaveNumber();
+    }
+
+    #region Updating HUD UI
+    private void UpdateCoreHealth()
+    {
+        coreFill.fillAmount = core.CurrentHealth / core.MaxHealth;
+    }
+
+    private void UpdateEnemyCount()
+    {
+        enemyCount.text = WaveManager.Instance.EnemiesRemaining.ToString();
+    }
+
+    public void UpdateWaveNumber()
+    {
+        waveNumber.text = WaveManager.Instance.WaveNumber.ToString();
+    }
+    #endregion
 
 }
