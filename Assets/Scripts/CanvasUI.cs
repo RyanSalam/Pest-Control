cf04c03e12917e16fec97c4450391ee1897fee8d
Joshua Smith <55558076+OhmikronStudios@users.noreply.github.com>
2021-01-30 22:05:06 -0500

    Main Menu functionality
    Added Character SO's
    All panels and buttons working to traverse the main menu, select a character. with dynamic text

--- Assets/Scripts/CanvasUI.cs
+++ Assets/Scripts/CanvasUI.cs
@@ -2,9 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CanvasUI : MonoBehaviour
 {
+    public Character character;
+
+    //Character Panel
+    [SerializeField] TextMeshProUGUI charName;
+    [SerializeField] TextMeshProUGUI charDesc;
+    [SerializeField] Image A1_image;
+    [SerializeField] TextMeshProUGUI A1_text;
+    [SerializeField] Image A2_image;
+    [SerializeField] TextMeshProUGUI A2_text;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +32,13 @@ public class CanvasUI : MonoBehaviour
 
     public void TurnObjectOn(GameObject obj)
     {
-            obj.SetActive(true);
+        obj.SetActive(true);
         Debug.Log("clicked");
     }
 
     public void TurnObjectOff(GameObject obj)
-    {
-      
+    {    
             obj.SetActive(false);
-
     }
 
     public void Quit()
@@ -35,7 +46,17 @@ public class CanvasUI : MonoBehaviour
         FindObjectOfType<GameManager>().OnQuitButton();
     }
 
-    
+    public void SelectCharacter(Character c)
+    {
+        character = c;
+        charName.text = c.c_name;
+        charDesc.text = c.c_desc;
+        A1_image = c.A1_image;
+        A1_text.text = c.A1_text;
+        A2_image = c.A2_image;
+        A1_text.text = c.A2_text;
+        
+    }
 
     
 }
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Character")]
+public class Character : ScriptableObject
+{
+    //Character Select Screen
+    public string c_name;
+    public string c_desc;
+
+    public Image A1_image;
+    public string A1_text;
+    public Image A2_image;
+    public string A2_text;
+
+    public Ability ab1;
+    public Ability ab2;
+
+
+
+
+
+}
