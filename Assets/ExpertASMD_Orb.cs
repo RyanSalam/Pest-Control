fab7d4b01d73040ff8068a4691846f2599b4302e
ClarkySnipe <61213916+ClarkySnipe@users.noreply.github.com>
2021-02-02 13:40:51 -0800

    Created a new gun (shock riffle)
    I called it the expert ASMD because its a replica from the gun from borderlands. Primary fire shoots 'bolts', alt fire shoots energy orbs. When you shoot the orbs with the bolt it explodes dealing massive damage

---
+++ Assets/ExpertASMD_Orb.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpertASMD_Orb : MonoBehaviour
+{
+    Rigidbody rb;
+    public float force = 5;
+
+    [SerializeField] GameObject explosionEffect;
+    [SerializeField] float explosionRadius; 
+    [SerializeField] float explosionDamage;
+    [SerializeField] LayerMask enemyMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        if (rb)
+        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //explosionss???
+        if (other.gameObject.tag == "ASMDammo")
+        {
+            Debug.Log("explosionss???");
+           
+            if (explosionEffect)
+            {
+                GameObject tempVFX = explosionEffect;
+                tempVFX.transform.localScale *= 3;
+                GameObject tempExplosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+                Destroy(tempExplosion, 1.5f);
+            }
+
+            explode(transform.position);
+        }
+    }
+
+    private void explode(Vector3 hitPoint)
+    {
+        Debug.Log("yes explosionss");
+        //sphere cast for enemies
+        Destroy(gameObject);
+
+        Collider[] colliders = Physics.OverlapSphere(hitPoint, explosionRadius, enemyMask);
+
+        foreach(Collider c in colliders)
+        {
+            Actor_Enemy tempEnemy = c.gameObject.GetComponent<Actor_Enemy>();
+
+            if (tempEnemy)
+                tempEnemy.TakeDamage(explosionDamage);
+        }
+    }
+}
