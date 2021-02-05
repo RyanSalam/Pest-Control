158049017808b23dfa90f572e856edf795a982c2
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-02-04 22:28:36 -0500

    lasertrap stuff

--- Assets/Scripts/Trap_Laser.cs
+++ Assets/Scripts/Trap_Laser.cs
@@ -7,6 +7,7 @@ public class Trap_Laser : Trap
     // Start is called before the first frame update
     //[SerializeField] float TrapDamage;
     private Actor_Enemy enemyTarget;
+    [SerializeField] LayerMask enemyMask;
     //Animator animator;
 
     public override void Activate()
@@ -15,20 +16,44 @@ public class Trap_Laser : Trap
         {
             return;
         }
-        base.Activate();
+        Debug.Log("Trap Active");
         //animator.SetTrigger("Lasers");
         Debug.Log("FUCK");
         enemyTarget.TakeDamage(trapDamage); //damage enemy 
         enemyTarget = null; //reseting enemy back to null
-        //base.Activate();
+        base.Activate();
     }
+
+    protected  override  void Update()
+    {
+        base.Update();
+
+        if (Physics.BoxCast(transform.position, Vector3.one * 3, Vector3.up, Quaternion.identity, 3f, enemyMask))
+        {
+            Debug.Log("DOUBLEFUCK");
+            Activate(); //when triggered activate
+        }
+    }
+
     private void OnTriggerEnter(Collider trigger)
     {
-        Debug.Log("DOUBLEFUCK");
-        enemyTarget = GetComponent<Actor_Enemy>();
-        if (enemyTarget != null) //if the enemy actor collides with trap
+        
+        //enemyTarget = GetComponent<Actor_Enemy>();
+        if (trigger.gameObject.tag == "Enemy") //if the enemy actor collides with trap
         {
-            Activate(); //when triggered activate
+            /*
+            Debug.Log("Triggered");
+            enemyTarget = trigger.gameObject.GetComponent<Actor_Enemy>();
+            if(enemyTarget != null)
+            {
+                Debug.Log("DOUBLEFUCK");
+                Activate(); //when triggered activate
+            }
+            else
+            {
+                Debug.Log("Enemys null");
+            }
+           */
         }
     }
 }
