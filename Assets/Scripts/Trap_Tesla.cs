fce0071bce720909151b7591f3b26bfb6e3c95da
Marko <romic17@gmail.com>
2021-02-20 20:14:40 -0500

    Tesla Model Import Correction

--- Assets/Scripts/Trap_Tesla.cs
+++ Assets/Scripts/Trap_Tesla.cs
@@ -7,7 +7,7 @@ public class Trap_Tesla : Trap
     private bool isChaining = false;
     private int currentChainAmmount = 0; // deafult chain value 
     [SerializeField] private int enemyChainAmmount = 4; // setting how much enemy can be infliceted by lighting chain
-    [SerializeField] private float chainRadius = 2.0f; //raidus of the chain
+    public float chainRadius = 2.0f; //raidus of the chain
     [SerializeField] private LayerMask whatIsEnemy; //check enemy layer 
     private Actor_Enemy enemyTarget;
 
