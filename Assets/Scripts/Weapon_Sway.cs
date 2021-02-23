5a5c510d738471441c9004d50c9fea3f33b7e06a
Kevin Vickers <dreammchine64@hotmail.com>
2021-02-23 17:19:20 -0500

    Starting the weapon sway script
    Lost some code from a power outage.

---
+++ Assets/Scripts/Weapon_Sway.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Sway : MonoBehaviour
+{
+    public float swayAmmount;
+    public float smoothing;
+
+    public Vector3 offsetMove;
+    private Vector3 initialPosition;
+   
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
