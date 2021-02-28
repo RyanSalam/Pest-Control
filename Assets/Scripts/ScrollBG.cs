0597bfc876dd404847c257f09879eae5d9152bfd
Kevin Vickers <dreammchine64@hotmail.com>
2021-02-27 20:48:40 -0500

    Added a scrolling background

---
+++ Assets/Scripts/ScrollBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBG : MonoBehaviour
+{
+
+    public float scrollSpeed = -5f;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 2000f);
+        transform.position =  Vector2.right * newPos + startPos;
+    }
+}
