b469e347fb2b1883ace2f684db4dd9b6882d6fbb
ClarkySnipe <61213916+ClarkySnipe@users.noreply.github.com>
2021-02-27 17:06:59 -0800

    Added Damage falloff for hitscan weapons
    Have not applied it to the actual damage yet, I want to see what MR.Salam has to say. Its pretty penalizing when you are far away so well see. If you uncomment the final debug in CalculateDamageFalloff function you can see the values its producing in game. Up for discussion as always.
    Also improved decals a bit.

--- Assets/Scripts/Enemy_DroneV2.cs
+++ Assets/Scripts/Enemy_DroneV2.cs
@@ -57,10 +57,12 @@ public class Enemy_DroneV2 : Actor_Enemy
             //checking if our hitpoint was above or below our y position. so we know if we should move below or above the obstacle in our way
             if (hit.point.y > gameObject.transform.position.y)//go down
             {
+               // Debug.Log("go down - ypos: " + gameObject.transform.position.y + " hit point: " + hit.point.y );
                 agent.baseOffset -= 0.4f;
             }
             else if (hit.point.y < gameObject.transform.position.y) //go up
             {
+                //Debug.Log("go up - ypos: " + gameObject.transform.position.y + " hit point: " + hit.point.y);
                 agent.baseOffset += 0.4f;
             }
 
@@ -125,11 +127,11 @@ public class Enemy_DroneV2 : Actor_Enemy
         base.Update();
     }
 
-    //private void OnDrawGizmosSelected()
-    //{
-    //    Gizmos.color = Color.yellow;
-    //    Gizmos.DrawWireSphere(collisionChecker.position, 1.0f);
-    //}
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(collisionChecker.position, 1.0f);
+    }
 
     public Transform searchForTarget()
     {
