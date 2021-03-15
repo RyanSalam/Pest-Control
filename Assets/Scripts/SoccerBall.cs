b62543c242a2ebd123b9c6d72c2079e6a8318e4d
Nayeem <60749652+Areteus@users.noreply.github.com>
2021-03-14 21:34:55 -0400

    Merge branch 'Development' into ElectricTrap-Model&Animations

---
+++ Assets/Scripts/SoccerBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBall : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    Rigidbody rb;
+    public float projectileForce = 5;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (!rb)
+            Debug.Log("Rigidbody not found on soccerball");
+    }
+    private void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.layer != groundLayer)
+        {
+            rb.AddForce(c.GetContact(0).normal * (projectileForce * 0.5f), ForceMode.Impulse);
+        }
+    }
+}
